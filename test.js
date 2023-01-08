907e778d4169dad37b6e995eb9efa47fa8ebca04
Dan Stronge <dnlstronge@gmail.com>
2023-01-08 14:04:36 +0000

    practice classes

--- test.js
+++ test.js
@@ -2,4 +2,11 @@ class Person {
     constructor() {
         this.name = "Dan"
     }
+    printName() {
+        console.log(this.name)
+    }
 }
+
+const person = new Person();
+person.printName()
+
